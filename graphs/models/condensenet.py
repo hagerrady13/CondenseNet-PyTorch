38985b820d02574da670ea096348bcbbc7e87f4f
Hager Rady <10363680+hagerrady13@users.noreply.github.com>
2018-05-13 00:22:25 +0200

    model added

--- graphs/models/condensenet.py
+++ graphs/models/condensenet.py
@@ -10,16 +10,75 @@ import json
 from easydict import EasyDict as edict
 import numpy as np
 
-from graphs.weights_initializer import fcn8s_weights_init
+from graphs.weights_initializer import init_model_weights
+from graphs.models.denseblock import DenseBlock
+from graphs.models.layers import LearnedGroupConv
 
 class CondenseNet(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
 
+        self.stages = self.config.stages
+        self.growth_rate = self.config.growth_rate
+        assert len(self.stages) == len(self.growth_rate)
 
-    def forward(self, x):
-        pass
+        self.init_stride = self.config.init_stride
+        self.pool_size = self.config.pool_size
+        self.num_classes = self.config.num_classes
+
+        self.progress = 0.0
+        self.num_filters = 2 * self.growth_rate[0]
+        """
+        Initializing layers
+        """
+        self.transition_pool = nn.AvgPool2d(kernel_size=2, stride=2)
+
+        self.last_pool = nn.AvgPool2d(self.pool_size)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.init_conv = nn.Conv2d(in_channels=self.config.input_channels, out_channels=self.num_filters, kernel_size=3, stride=self.init_stride, padding=1, bias=False)
+
+        self.denseblock_one = DenseBlock(num_layers=self.stages[0], in_channels= self.num_filters, growth_rate=self.growth_rate[0])
+
+        self.num_filters += self.stages[0] * self.growth_rate[0]
+
+        self.denseblock_two = DenseBlock(num_layers=self.stages[1], in_channels= self.num_filters, growth_rate=self.growth_rate[1])
+
+        self.num_filters += self.stages[1] * self.growth_rate[1]
+
+        self.denseblock_three = DenseBlock(num_layers=self.stages[2], in_channels= self.num_filters, growth_rate=self.growth_rate[2])
+
+        self.num_filters += self.stages[2] * self.growth_rate[2]
+        self.last_bn = nn.BatchNorm2d(self.num_filters)
+
+        self.classifier = nn.Linear(self.num_filters, self.num_classes)
+
+        self.apply(init_model_weights)
+
+    def forward(self, x, progress=None):
+        if progress:
+            LearnedGroupConv.global_progress = progress
+
+        out = self.init_conv(x)
+
+        out = self.denseblock_one(out)
+        out = self.transition_pool(out)
+
+        out = self.denseblock_two(out)
+        out = self.transition_pool(out)
+
+        out = self.denseblock_three(out)
+        out = self.last_bn(out)
+        out = self.relu(out)
+        out = self.last_pool(out)
+
+        out = self.view(out.size(0), -1)
+
+        out = self.classifier(out)
+
+        return out
+
 """
 Testing model
 """
@@ -40,10 +99,6 @@ def main():
     inp = np.expand_dims(inp, axis=0)
 
     inp = torch.autograd.Variable(torch.from_numpy(inp).float())
-    FCN =  CondenseNet(model, 5)
-    out = FCN(inp)
-
-    print (out)
 
 if __name__ == '__main__':
     main()
---
+++ graphs/models/layers.py
@@ -0,0 +1,9 @@
+"""
+Definitions for custom layers and blocks
+"""
+import torch.nn as nn
+
+class LearnedGroupConv(nn.Module):
+    pass
+
+
