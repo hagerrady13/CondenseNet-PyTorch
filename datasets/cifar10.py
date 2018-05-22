ff547bfdfbb654295858121e791cc26db3cb730d
Hager Rady <10363680+hagerrady13@users.noreply.github.com>
2018-05-22 06:00:34 +0200

    updated utils and data loader

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -15,34 +15,27 @@ class Cifar10DataLoader:
         if config.data_mode == "numpy_train":
 
             print("Loading DATA.....")
-            train_data = torch.from_numpy(np.load(config.data_folder + config.x_train)).float()
-            train_labels = torch.from_numpy(np.load(config.data_folder + config.y_train)).long()
-            valid_data = torch.from_numpy(np.load(config.data_folder + config.x_valid)).float()
-            valid_labels = torch.from_numpy(np.load(config.data_folder + config.y_valid)).long()
+            normalize = v_transforms.Normalize(mean=[0.4914, 0.4824, 0.4467],
+                                             std=[0.2471, 0.2435, 0.2616])
 
-            self.len_train_data = train_data.size()[0]
-            self.len_valid_data = valid_data.size()[0]
+            train_set = v_datasets.CIFAR10('./data', train=True, download=True,
+                                         transform=v_transforms.Compose([
+                                             v_transforms.RandomCrop(32, padding=4),
+                                             v_transforms.RandomHorizontalFlip(),
+                                             v_transforms.ToTensor(),
+                                             normalize,
+                                         ]))
+            valid_set = v_datasets.CIFAR10('./data', train=False,
+                                       transform=v_transforms.Compose([
+                                           v_transforms.ToTensor(),
+                                           normalize,
+                                       ]))
 
-            self.train_iterations = (self.len_train_data + self.config.batch_size - 1) // self.config.batch_size
-            self.valid_iterations = (self.len_valid_data + self.config.batch_size - 1) // self.config.batch_size
+            self.train_loader = DataLoader(train_set, batch_size=self.config.batch_size, shuffle=True)
+            self.valid_loader = DataLoader(valid_set, batch_size=self.config.batch_size, shuffle=False)
 
-            print("""
-Some Statistics about the training data
-train_data shape: {}, type: {}
-train_labels shape: {}, type: {}
-valid_data shape: {}, type: {}
-valid_labels type: {}, type: {}
-train_iterations: {}
-valid_iterations: {}
-            """.format(train_data.size(), train_data.type(), train_labels.size(), train_labels.type(),
-                       valid_data.size(), valid_data.type(), valid_labels.size(), valid_labels.type(),
-                       self.train_iterations, self.valid_iterations))
-
-            train = TensorDataset(train_data, train_labels)
-            valid = TensorDataset(valid_data, valid_labels)
-
-            self.train_loader = DataLoader(train, batch_size=config.batch_size, shuffle=True)
-            self.valid_loader = DataLoader(valid, batch_size=config.batch_size, shuffle=False)
+            self.train_iterations = len(self.train_loader)
+            self.valid_loader = len(self.valid_loader)
 
         elif config.data_mode == "numpy_test":
             test_data = torch.from_numpy(np.load(config.data_folder + config.x_test)).float()
@@ -65,8 +58,8 @@ test_iterations: {}
             self.test_loader = DataLoader(test, batch_size=config.batch_size, shuffle=False)
 
         elif config.data_mode == "random":
-            train_data = torch.randn(2, self.config.num_channels, self.config.img_size, self.config.img_size)
-            train_labels = torch.ones(2, self.config.img_size, self.config.img_size).long()
+            train_data = torch.randn(8, self.config.input_channels, self.config.img_size, self.config.img_size)
+            train_labels = torch.ones(8).long()
             valid_data = train_data
             valid_labels = train_labels
             self.len_train_data = train_data.size()[0]
